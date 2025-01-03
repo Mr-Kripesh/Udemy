da40f9621d8e153696971b775b51c6705ddda4d2
Mrkripesh <kripeshpanta44@gmai.com>
2025-01-04 02:13:43 +0545

    Simon Game Completed.

--- The Simon Game/style.js
+++ The Simon Game/style.js
@@ -10,17 +10,50 @@ $(document).keypress(function() {
        nextSequence();
        started = true;
     }
-
 });
 
-$(".btn").click(function(){
+$(".btn").click(function() {
+    if(!started){
+        $("body").addClass("game-over");
+        playSound("wrong");
+        $("h1").text("Game Over, Press Any Key to Restart");
+        startOver();
+        return;
+    }
+    else{
     var userChosenColor = $(this).attr("id");
     userClickedPattern.push(userChosenColor);
     playSound(userChosenColor);
     animatePress(userChosenColor);
-   });
+    checkAnswer(userClickedPattern.length - 1);
+   }
 
+});
+
+function checkAnswer(currentLevel) {
+    if (gamePattern[currentLevel]===userClickedPattern[currentLevel]) {
+        console.log("success");
+    
+        if (userClickedPattern.length===gamePattern.length) {
+            setTimeout(function() {
+               nextSequence(); 
+            }, 1000);
+            
+        }
+    }
+    else{
+        playSound("wrong");
+        $("body").addClass("game-over");
+        $("h1").text("Game Over, Press Any Key to Restart");
+        setTimeout(function() {
+            $("body").removeClass("game-over");
+        }, 200);
+        
+        startOver();
+    }
+}
 function nextSequence() {
+    userClickedPattern =[];
     level++;
     $("h1").text("Level "+ level);
 
@@ -34,12 +67,6 @@ function nextSequence() {
 
 }
 
-function playSound(name){
-
-     var audio = new Audio("sounds/" + name + ".mp3");
-     audio.play();
-}
-
 function animatePress(currentColor) {
     $("#" + currentColor).addClass("pressed");
 
@@ -48,7 +75,19 @@ function animatePress(currentColor) {
     }, 100);
 }
 
+function playSound(name){
 
+     var audio = new Audio("sounds/" + name + ".mp3");
+     audio.play();
+}
+
+
+
+function startOver(){
+    level = 0;
+    gamePattern = [];
+    started = false;
+}
 
 // For green Button
 
